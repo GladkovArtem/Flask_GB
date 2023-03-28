2a906d727d4cc3397eab8ba9550c870593c7d7f8
ArtemGladkov <2020artem@mail.ru>
2023-03-28 15:37:29 +0300

    DZ10_GladkovArtem_Flask

--- blog/api/author.py
+++ blog/api/author.py
@@ -1,7 +1,8 @@
 from flask_combo_jsonapi import ResourceDetail, ResourceList
 from blog.schemas import AuthorSchema
 from blog.database import db
-from blog.models import Author
+from blog.models import Author, Articles
+from combojsonapi.event.resource import EventsResource
 
 
 class AuthorList(ResourceList):
@@ -12,7 +13,13 @@ class AuthorList(ResourceList):
     }
 
 
+class AuthorDetailEvents(EventsResource):
+    def event_get_articles_count(self, **kwargs):
+        return {"count": Articles.query.filter(Articles.author_id == kwargs["id"]).count()}
+
+
 class AuthorDetail(ResourceDetail):
+    events = AuthorDetailEvents
     schema = AuthorSchema
     data_layer = {
         "session": db.session,
