b3d5a143d268c2d567a94bcf3ab2df5d9c61cf28
ArtemGladkov <2020artem@mail.ru>
2023-03-29 18:55:02 +0300

    DZ11_17_GladkovArtem_Flask

---
+++ migrations/versions/a77229808eae_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a77229808eae
+Revises: 7d98d85c5513
+Create Date: 2023-03-29 15:35:11.094312
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a77229808eae'
+down_revision = '7d98d85c5513'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
