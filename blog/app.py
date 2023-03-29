a04b8989ad262fbe3fa3e93f94ed2d084dfa627f
ArtemGladkov <2020artem@mail.ru>
2023-03-29 12:22:13 +0300

    DZ11_5_GladkovArtem_Flask

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,6 @@
 from blog.articles.views import articles_app
 from blog.author.views import authors
+from blog.configs import DevConfig
 from blog.database import db
 from blog.user.views import users
 from flask import Flask, render_template
@@ -11,9 +12,8 @@ from blog.admin.admin import admin
 from blog.api import init_api
 
 
-cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
 app = Flask(__name__)
-app.config.from_object(f"blog.configs.{cfg_name}")
+app.config.from_object(DevConfig)
 app.register_blueprint(users, url_prefix="/users")
 app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(articles_app, url_prefix="/articles")
